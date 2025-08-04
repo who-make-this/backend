615995745b762397ce3b17a2ba6a1cc6d397beef
PARK, Yeonjong <yeonjong.park@outlook.com>
2025-08-04 14:55:02 +0900

    init: 초기 환경을 구성한다

---
+++ src/main/java/university/likelion/wmt/WmtApplication.java
@@ -0,0 +1,13 @@
+package university.likelion.wmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/university/likelion/wmt/WmtApplicationTests.java
@@ -0,0 +1,13 @@
+package university.likelion.wmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
