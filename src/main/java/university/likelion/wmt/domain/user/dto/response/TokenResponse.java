546908fef66403b084861ba10e3db08f201d6baa
PARK, Yeonjong <yeonjong.park@outlook.com>
2025-08-14 20:08:02 +0900

    modify: 검증 애너테이션 추가

--- src/main/java/university/likelion/wmt/domain/user/dto/response/TokenResponse.java
+++ src/main/java/university/likelion/wmt/domain/user/dto/response/TokenResponse.java
@@ -1,6 +1,11 @@
 package university.likelion.wmt.domain.user.dto.response;
 
 public record TokenResponse(
-    String accessToken
+    String accessToken,
+    String tokenType,
+    long expiresIn
 ) {
+    public TokenResponse(String accessToken) {
+        this(accessToken, "Bearer", 1209600L);
+    }
 }
--- src/main/java/university/likelion/wmt/domain/user/dto/response/UserInfoResponse.java
+++ src/main/java/university/likelion/wmt/domain/user/dto/response/UserInfoResponse.java
@@ -2,19 +2,13 @@ package university.likelion.wmt.domain.user.dto.response;
 
 import java.time.LocalDate;
 
-import lombok.Builder;
-
 import university.likelion.wmt.domain.user.entity.User;
 
-@Builder
 public record UserInfoResponse(
     String nickname,
     LocalDate createdAt
 ) {
     public static UserInfoResponse from(User user) {
-        return UserInfoResponse.builder()
-            .nickname(user.getNickname())
-            .createdAt(user.getCreatedAt().toLocalDate())
-            .build();
+        return new UserInfoResponse(user.getNickname(), user.getCreatedAt().toLocalDate());
     }
 }
