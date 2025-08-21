0f0b4ce5a7e4441974330f36e357a87720a24c17
PARK, Yeonjong <yeonjong.park@outlook.com>
2025-08-21 13:28:27 +0900

    ㅇㅇ

--- src/main/java/university/likelion/wmt/WmtApplication.java
+++ src/main/java/university/likelion/wmt/WmtApplication.java
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class WmtApplication {
 
     public static void main(String[] args) {
+        System.out.println("!!");
         SpringApplication.run(WmtApplication.class, args);
     }
 
