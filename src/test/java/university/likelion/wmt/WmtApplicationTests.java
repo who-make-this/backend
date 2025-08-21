ca0303678ac0200af9936702eb2e75ac7f2c9861
PARK, Yeonjong <yeonjong.park@outlook.com>
2025-08-21 13:21:15 +0900

    ㅇㅇ

--- src/test/java/university/likelion/wmt/WmtApplicationTests.java
+++ src/test/java/university/likelion/wmt/WmtApplicationTests.java
@@ -2,8 +2,10 @@ package university.likelion.wmt;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WmtApplicationTests {
 
 	@Test
